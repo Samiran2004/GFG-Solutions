9d5924980c683f6638ae0d574adb34f86c199765
Zephyrus <130337647+Samiran2004@users.noreply.github.com>
2026-06-13 08:18:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Depth Of Binary Tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+
+class Node:
+
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        
+        return max(left_depth, right_depth) + 1
